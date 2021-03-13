e392898a773a84d8fa559d53bc1d6480c6bbeed4
raiyan29 <78214519+raiyan29@users.noreply.github.com>
2021-03-13 22:37:45 +0530

    make it bettter

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ const studentRouter =require('./routers/student')
 app.use('/students',studentRouter)
 var port =process.env.PORT||5000
 app.listen(port,function(){
-      console.log("Server started")
+      console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 
 })
